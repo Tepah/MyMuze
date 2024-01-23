e91a263161b9490df363059bc1e3630279a33f98
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2024-01-23 10:29:47 -0800

    Initial Commit

---
+++ MyMuze/MyMuzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMuzeApp.swift
+//  MyMuze
+//
+//  Created by Pete Potipitak on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMuzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
