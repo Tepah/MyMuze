08c9cc4933dc2c1e2b541a1210e705a1264a9a47
Diamond <diamondly@csu.fullerton.edu>
2024-02-22 23:35:23 -0800

    Linked mock usernames to user profile view

---
+++ MyMuze/HomeView/ProfileUI.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileUI.swift
+//  MyMuze
+//
+//  Created by Diamond Ly on 2/22/24.
+//
+
+import SwiftUI
+
+struct ProfileUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileUI()
+}
