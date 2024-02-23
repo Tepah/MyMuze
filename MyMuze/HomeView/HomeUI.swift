0846b7f18e9b15e6351786b10168ea97a0a4f58f
Diamond <diamondly@csu.fullerton.edu>
2024-02-22 22:22:44 -0800

    Added UserFeed using mock data

--- MyMuze/HomeView/HomeUI.swift
+++ MyMuze/HomeView/HomeUI.swift
@@ -13,14 +13,67 @@ struct HomeUI: View {
             .ignoresSafeArea()
             .overlay(
                 VStack {
-                    Text("Home")
+                    Text(" Home")
                         .foregroundColor(Color.white)
-                        .multilineTextAlignment(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                         .bold()
                         .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
                         .padding(.trailing, 250.0)
                     Divider()
                         .overlay(.white)
+                        .frame(height: 2)
+                        .background(Color.white)
+                    
+                    ZStack {
+                        VStack{
+                            Text("@diamondly")
+                                .foregroundColor(Color("Accent Color"))
+                                .frame(maxWidth: .infinity, alignment: .topLeading)
+                                .fontWeight(.bold)
+                                Text("THE END")
+                                    .foregroundColor(Color.white)
+                                    .frame(maxWidth: . infinity, alignment: .topLeading)
+                                Text("Alesso, Charlotte Lawrence")
+                                    .foregroundColor(Color.gray)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    Divider()
+                        .overlay(.white)
+                    
+                    ZStack {
+                        VStack{
+                            Text("@potipitak")
+                                .foregroundColor(Color("Accent Color"))
+                                .frame(maxWidth: .infinity, alignment: .topLeading)
+                                .fontWeight(.bold)
+                                Text("golden hour - Fujii Kaze Remix")
+                                    .foregroundColor(Color.white)
+                                    .frame(maxWidth: . infinity, alignment: .topLeading)
+                                Text("JVKE, Fujii Kaze")
+                                    .foregroundColor(Color.gray)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    Divider()
+                        .overlay(.white)
+                    ZStack {
+                        VStack{
+                            Text("@dave123")
+                                .foregroundColor(Color("Accent Color"))
+                                .frame(maxWidth: .infinity, alignment: .topLeading)
+                                .fontWeight(.bold)
+                                Text("Use Somebody")
+                                    .foregroundColor(Color.white)
+                                    .frame(maxWidth: . infinity, alignment: .topLeading)
+                                Text("Kings of Leon")
+                                    .foregroundColor(Color.gray)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    Divider()
+                        .overlay(.white)
+                    
                     Spacer()
                 }
         )
