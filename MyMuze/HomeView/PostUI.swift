5bc514ff48d34a4902ad82cb997a5f5a7b0a6694
Diamond <diamondly@csu.fullerton.edu>
2024-02-29 15:25:30 -0800

    Created PostUI

---
+++ MyMuze/HomeView/PostUI.swift
@@ -0,0 +1,18 @@
+//
+//  PostUI.swift
+//  MyMuze
+//
+//  Created by Diamond Ly on 2/29/24.
+//
+
+import SwiftUI
+
+struct PostUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostUI()
+}
