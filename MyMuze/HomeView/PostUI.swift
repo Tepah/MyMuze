836c2f6663347f92aa604749ec943f6866858129
Diamond <diamondly@csu.fullerton.edu>
2024-04-16 16:59:03 -0700

    implemented searchSpotify function

--- MyMuze/HomeView/PostUI.swift
+++ MyMuze/HomeView/PostUI.swift
@@ -26,6 +26,25 @@ struct PostUI: View {
                 }
             )
     }
+    
+    func searchSpotify() {
+        let spotifyClient = SpotifyAPIClient()
+        spotifyClient.getAccessToken { accessToken in
+            if let token = accessToken {
+                print("Access Token: \(token)")
+                // Use the access token to make further requests to Spotify API
+                spotifyClient.searchTracks(query: "Shawn Mendes", accessToken: token) { tracks in
+                    if let tracks = tracks {
+                        print("Found Tracks: \(tracks)")
+                    } else {
+                        print("Failed to get tracks")
+                    }
+                }
+            } else {
+                print("Failed to get access token")
+            }
+        }
+    }
 }
 
 #Preview {
