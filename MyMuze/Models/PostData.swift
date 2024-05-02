203680272b0e4e52b254bc747b61fdcdf44f64be
Diamond <diamondly@csu.fullerton.edu>
2024-05-01 19:06:19 -0700

    Created new model to hold post data

--- MyMuze/Models/PostData.swift
+++ MyMuze/Models/PostData.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct PostData: Codable {
+    var uid: String
+    var username: String
+    var timestamp: String
+    var track: String
+    var likes: Int?
+    var comments: [String]?
+//    var postID: String?
+    
+    func toDictionary() -> [String: Any] {
+        var dictionary: [String: Any] = [:]
+        dictionary["username"] = username
+        dictionary["timestamp"] = timestamp
+        dictionary["uid"] = uid
+        dictionary["track"] = track
+        
+        if let likes = likes {
+            dictionary["likes"] = likes
+        }
+        if let comments = comments {
+            dictionary["comments"] = comments
+        }
+//        if let postID = postID {
+//            dictionary["postID"] = postID
+//        }
+
+        return dictionary
+    }
+}
