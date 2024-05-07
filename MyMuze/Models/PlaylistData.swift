ec10676ae6cc355916d4977f8959341f3f26da50
Diamond <diamondly@csu.fullerton.edu>
2024-05-07 14:09:37 -0700

    Created new model and view for playlist creation

---
+++ MyMuze/Models/PlaylistData.swift
@@ -0,0 +1,8 @@
+//
+//  PlaylistData.swift
+//  MyMuze
+//
+//  Created by Diamond Ly on 5/6/24.
+//
+
+import Foundation
---
+++ MyMuze/Views/BuildPlaylistView.swift
@@ -0,0 +1,18 @@
+//
+//  BuildPlaylistView.swift
+//  MyMuze
+//
+//  Created by Diamond Ly on 5/6/24.
+//
+
+import SwiftUI
+
+struct BuildPlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuildPlaylistView()
+}
