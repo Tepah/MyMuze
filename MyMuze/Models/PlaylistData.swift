2323187634c48f0862ac622cc9116dcabb04278c
Diamond <diamondly@csu.fullerton.edu>
2024-05-07 14:11:09 -0700

    Created a model to hold playlist data from Firestore

--- MyMuze/Models/PlaylistData.swift
+++ MyMuze/Models/PlaylistData.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct PlaylistData: Codable {
+    var id = UUID()
+    var uid: String
+    var tracks: [String]
+    
+    func toDictionary() -> [String: Any] {
+        var dictionary: [String: Any] = [
+            "uid": uid,
+            "tracks": tracks,
+        ]
+
+        return dictionary
+    }
+}
